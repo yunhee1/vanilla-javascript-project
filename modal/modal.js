492574a9cf9eb10d56addedf223f0ec2fba38cee
yunhee1 <alkrg007@naver.com>
2023-05-04 12:12:15 +0900

    스크립트 연결 링크 수정

--- modal/modal.js
+++ modal/modal.js
@@ -2,3 +2,14 @@ const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+
+modal.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+})
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
