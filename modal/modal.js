03a8d3dc7e70c703d2cf8ad06ca9d51e6739d55d
yunhee1 <alkrg007@naver.com>
2023-05-04 16:08:39 +0900

    모달창 밖 누를 때만 모달 닫기

--- modal/modal.js
+++ modal/modal.js
@@ -1,13 +1,19 @@
 const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
+const modalContainer = document.querySelector(".modal-container");
 
 modalBtn.addEventListener("click", function () {
   modal.classList.add("open-modal");
 });
 
-modal.addEventListener("click", function () {
-  modal.classList.remove("open-modal");
+modal.addEventListener("click", e => {
+  const evTarget = e.target
+  if ( evTarget.classList.contains("modal-container") ) {
+  modal.classList.add("open-modal");
+  }
+  else {
+    modal.classList.remove("open-modal");}
 })
 
 closeBtn.addEventListener("click", function () {
